42bbcb8dc1e21956c829cece1897246307a0ece4
Realzy <SN032833@firstbanknigeria.com>
2025-01-31 13:40:07 +0100

    Changed the data-type to accomodate the order required /task checker

--- handlers/api.go
+++ handlers/api.go
@@ -7,11 +7,27 @@ import (
 	"github.com/Tonyrealzy/Basic-Information-Public-API/config"
 )
 
+// Define a struct to maintain key order
+type BasicInfoResponse struct {
+	Email            string `json:"email"`
+	CurrentTimestamp string `json:"current_timestamp"`
+	GithubURL        string `json:"github_url"`
+}
+
 func GetBasicInfo(c *gin.Context) {
-	response:= map[string]string {
-		"email": config.Email,
-		"current_timestamp": time.Now().UTC().Format(time.RFC3339),
-		"github_url": config.GithubURL,
+	response := BasicInfoResponse{
+		Email:            config.Email,
+		CurrentTimestamp: time.Now().UTC().Format(time.RFC3339),
+		GithubURL:        config.GithubURL,
 	}
 	c.JSON(http.StatusOK, response)
 }
+
+// func GetBasicInfo(c *gin.Context) {
+// 	response:= map[string]string {
+// 		"email": config.Email,
+// 		"current_timestamp": time.Now().UTC().Format(time.RFC3339),
+// 		"github_url": config.GithubURL,
+// 	}
+// 	c.JSON(http.StatusOK, response)
+// }
